8b9abb63e3f85db22b597c4ad983fa24e0d3f5c2
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-05-21 00:31:47 +0530

    2336

---
+++ 2336.cpp
@@ -0,0 +1,39 @@
+class SmallestInfiniteSet {
+    int start;
+    set<int> st;
+public:
+
+    SmallestInfiniteSet() {
+        start=1;
+    }
+    
+    int popSmallest() {
+        if(st.empty()){
+            start++;
+            return start-1;
+        }
+        else{
+            int s=*st.begin();
+            if(s<start){
+                st.erase(st.begin());
+                return s;
+            }
+            else{
+                start++;
+                return start-1;
+            }
+        }
+    }
+    void addBack(int num) {
+        if (num<start){
+            st.insert(num);
+        }
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
