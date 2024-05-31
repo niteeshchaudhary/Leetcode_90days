75f715d2526e0b2d14479d5e5542f0a89a935e29
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-05-31 23:58:43 +0530

    66

---
+++ 66.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>-1;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }
+            else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
