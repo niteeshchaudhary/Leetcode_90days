12dfb2b78d2bc5bb2385b614c024cf09088d5a6c
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-07-06 13:49:03 +0530

    2582

---
+++ 2582.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int q=time/(n-1);
+        int r=time%(n-1);
+        if(q&1){
+            if(r==0){
+                return n;
+            }
+            return n-(r);
+        }else{
+            return r+1;
+        }
+        
+    }
+};
