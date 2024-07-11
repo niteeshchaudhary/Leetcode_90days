db159884296c671ef4d7bd7ae738ae60274518bc
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-07-12 01:39:34 +0530

    202

---
+++ 202.cpp
@@ -0,0 +1,23 @@
+#include <unordered_set>
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> visited;
+        visited.insert(n);
+        while(n!=1){
+            int temp=n;
+            n=0;
+            while(temp!=0){
+                n+=(temp%10)*(temp%10);
+                temp/=10;
+            }
+            if(visited.count(n)){
+                return false;
+            }
+            visited.insert(n);
+
+        }
+        return true;
+        
+    }
+};
