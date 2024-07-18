c3b73633627b021d73f480df9030a908f6f81bc0
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-07-18 23:58:50 +0530

    1530

--- 1530.cpp
+++ 1530.cpp
@@ -1,14 +1,15 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-  struct TreeNode {
+
+struct TreeNode {
       int val;
       TreeNode *left;
       TreeNode *right;
      TreeNode() : val(0), left(nullptr), right(nullptr) {}
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
+};
  
 class Solution {
 public:
